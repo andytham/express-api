cbd3624af8d59ece5a81d0f21150ec0d348f8f05
andy <thamandyy@gmail.com>
2018-06-25 23:31:00 -0400

    added cors methods, fixed table migration

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
 const www = process.env.WWW || './';
 app.use(express.static(www));
 console.log(`serving ${www}`);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 // app.get('*', (req, res) => {
 // 	res.sendFile(`index.html`, { root: www });
 // });
 
-//chat.andytham.com
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "PATCH, POST")
+  next();
+});
+
+// chat.andytham.com
 const historyRoutes = require('./routes/history-route');
 const statusesRoutes = require('./routes/statuses-route');
 app.use('/api/history', historyRoutes);
--- db/migrations/history-migration.sql
+++ db/migrations/history-migration.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS chat_history(
     id SERIAL,
-    usr VARCHAR(100) UNIQUE NOT NULL,
+    usr VARCHAR(100),
     msg TEXT,
     time VARCHAR(50)
 );
