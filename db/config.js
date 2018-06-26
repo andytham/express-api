94e58ebb943c6d54d3439c1165023c3dc550f04d
andy <thamandyy@gmail.com>
2018-06-25 20:33:39 -0400

    db config

---
+++ db/config.js
@@ -0,0 +1,24 @@
+const options = {
+	query: (event) => {
+		console.log(event.query)
+	}
+}
+
+const pgp = require('pg-promise')(options);
+
+const setDb = () => {
+	if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV){
+		return pgp({
+			database: "express-api",
+			port: 5432,
+			host: "localhost"
+		})
+	} 
+	else if (process.envNODE_ENV === "production") {
+		return pgp(process.env.DATABASE_URL)
+	}
+}
+
+const db = setDb();
+
+module.exports = db;
